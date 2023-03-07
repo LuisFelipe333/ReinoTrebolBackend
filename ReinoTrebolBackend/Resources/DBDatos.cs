9eb149a109297d314e5a3964255088e70aabc873
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-07 15:18:08 -0600

    Added Connections

---
+++ ReinoTrebolBackend/Resources/DBDatos.cs
@@ -0,0 +1,121 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Reflection.Metadata;
+
+namespace ReinoTrebolBackend.Resources
+{
+    public class DBDatos
+    {
+        //public static string cadenaConexion = "Data Source=.;Initial Catalog=Test;User ID=angels;Password=123.";
+        public static string cadenaConexion = "Data Source=.;Initial Catalog=test;User ID=sa;Password=pokemon3";
+        public static DataSet ListarTablas(string nombreProcedimiento, List<Parametro> parametros = null)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand(nombreProcedimiento, conexion);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                if (parametros != null)
+                {
+                    foreach (var parametro in parametros)
+                    {
+                        cmd.Parameters.AddWithValue(parametro.Nombre, parametro.Valor);
+                    }
+                }
+                DataSet tabla = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public static DataTable Listar(string nombreProcedimiento, List<Parametro> parametros = null)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand(nombreProcedimiento, conexion);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                if (parametros != null)
+                {
+                    foreach (var parametro in parametros)
+                    {
+                        cmd.Parameters.AddWithValue(parametro.Nombre, parametro.Valor);
+                    }
+                }
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public static dynamic Ejecutar(string nombreProcedimiento, List<Parametro> parametros = null)
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand(nombreProcedimiento, conexion);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                if (parametros != null)
+                {
+                    foreach (var parametro in parametros)
+                    {
+                        cmd.Parameters.AddWithValue(parametro.Nombre, parametro.Valor);
+                    }
+                }
+
+                int i = cmd.ExecuteNonQuery();
+
+                bool exito = (i > 0) ? true : false;
+
+                return new
+                {
+                    exito = exito,
+                    message = "Exito"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    exito = false,
+                    message = ex.Message
+                };
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
---
+++ ReinoTrebolBackend/Resources/Parametro.cs
@@ -0,0 +1,15 @@
+﻿namespace ReinoTrebolBackend.Resources
+{
+    public class Parametro
+    {
+        public string Nombre { get; set; }
+        public string Valor { get; set; }
+
+        public Parametro(string nombre, string valor)
+        {
+            Nombre = nombre;
+            Valor = valor;
+        }
+
+    }
+}
